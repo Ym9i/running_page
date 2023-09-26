33ec19d1de47f1eac561abf4828d7ce629eae914
Ym9i <971822071@qq.com>
2023-09-26 16:15:18 +0800

    chang site meda

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,18 +11,10 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  siteUrl: 'https://bobrun.vercel.app/',
+  logo: 'https://tbgnu8og-1300158435.cos.ap-shanghai.myqcloud.com/running_page/logo.jpg',
   description: 'Personal site and blog',
   navLinks: [
-    {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
-    {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
-    },
   ],
 };
 
