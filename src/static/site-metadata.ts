27effcbb40e07d5ba188dc87b960ed944ae15b58
Bob <bob.zhao@data-whisper.com>
2026-04-28 18:34:31 +0800

    add develop branch for code update gp_bob for vercel only

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -15,24 +15,11 @@ const getBasePath = () => {
 };
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  siteTitle: 'BOb Running Page',
+  siteUrl: 'https://run.971822.xyz/',
+  logo: 'https://tbgnu8og-1300158435.cos.ap-shanghai.myqcloud.com/running_page/logo.jpg',
   description: 'Personal site and blog',
-  navLinks: [
-    {
-      name: 'Summary',
-      url: `${getBasePath()}/summary`,
-    },
-    {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
-    {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
-    },
-  ],
+  navLinks: [],
 };
 
 export default data;
